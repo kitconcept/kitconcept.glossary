379b8431e4186d063270df60fd807a9b16aac149
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2019-04-18 13:09:51 -0300

    Remove unused tests

--- src/kitconcept/glossary/tests/test_views.py
+++ src/kitconcept/glossary/tests/test_views.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from kitconcept.glossary.interfaces import IGlossarySettings
 from kitconcept.glossary.testing import INTEGRATION_TESTING
 from plone import api
 from plone.app.textfield.value import RichTextValue
@@ -57,54 +56,3 @@ class GlossaryViewTestCase(BaseViewTestCase):
     def setUp(self):
         super(GlossaryViewTestCase, self).setUp()
         self.view = api.content.get_view(u'view', self.g1, self.request)
-
-
-class GlossaryStateViewTestCase(BaseViewTestCase):
-
-    def setUp(self):
-        super(GlossaryStateViewTestCase, self).setUp()
-        self.view = api.content.get_view(u'glossary_state', self.portal, self.request)
-
-    def test_tooltip_is_enabled(self):
-        name = IGlossarySettings.__identifier__ + '.enable_tooltip'
-        api.portal.set_registry_record(name, True)
-        self.assertTrue(self.view.tooltip_is_enabled)
-
-        name = IGlossarySettings.__identifier__ + '.enable_tooltip'
-        api.portal.set_registry_record(name, False)
-        self.assertFalse(self.view.tooltip_is_enabled)
-
-    def test_content_type_is_enabled(self):
-        self.assertFalse(self.view.content_type_is_enabled)
-
-        self.view.context = self.d1
-        self.assertTrue(self.view.content_type_is_enabled)
-
-        self.view.context = self.g1
-        self.assertFalse(self.view.content_type_is_enabled)
-
-
-class JsonViewTestCase(BaseViewTestCase):
-
-    def setUp(self):
-        super(JsonViewTestCase, self).setUp()
-        self.view = api.content.get_view(
-            u'glossary', self.portal, self.request)
-
-    def test_get_json_entries(self):
-        expected = [
-            {'definition': 'First Term Description', 'term': 'First Term'},
-            {'definition': 'Second Term Description', 'term': 'Second Term'},
-        ]
-        self.assertEqual(self.view.get_json_entries(), expected)
-
-    def test__call__(self):
-        import json
-        self.view()
-        result = self.view.request.response.getBody()
-
-        expected = [
-            {'definition': 'First Term Description', 'term': 'First Term'},
-            {'definition': 'Second Term Description', 'term': 'Second Term'},
-        ]
-        self.assertEqual(json.loads(result), expected)
