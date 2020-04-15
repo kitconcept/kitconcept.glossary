55f4941748d61e706b810953d86e7777bdcbcffa
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-04-15 16:41:34 +0200

    Back to development: 1.0.1
    [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = "1.0.0"
+version = "1.0.1.dev0"
 description = "Plone add-on product to define a glossary."
 long_description = (
     open("README.rst").read()
